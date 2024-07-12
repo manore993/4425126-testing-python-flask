e134ea4b451a8971b839da195fb6e3f6de482d63
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-07-12 10:50:52 +0200

    Creation of the test_front_end.py and writting first test using playwright.

---
+++ tests/test_front_end.py
@@ -0,0 +1,7 @@
+import re
+from playwright.sync_api import Page, expect
+
+def test_access_homepage(page: Page):
+    page.goto("http://127.0.0.1:5000")
+
+    expect(page).to_have_title(re.compile("Home Page"))

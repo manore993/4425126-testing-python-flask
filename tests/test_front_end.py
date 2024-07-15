79bb9bddf21cd33ecba46abea5b46a7af51b35ca
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-07-15 16:32:06 +0200

    Code written to test "register username" functionality.

--- tests/test_front_end.py
+++ tests/test_front_end.py
@@ -6,3 +6,17 @@ def test_access_homepage(page: Page):
 
     expect(page).to_have_title(re.compile("Home Page"))
 
+def test_register_username(page: Page):
+    page.goto("http://127.0.0.1:5000")
+    expect(page).to_have_title(re.compile("Home Page"))
+
+    page.get_by_role("link", name="Register", exact=True).click()
+    expect(page.locator("h3")).to_contain_text(re.compile("Register User"))
+
+    page.get_by_role("textbox", name="name").fill("Hulu")
+    page.get_by_role("textbox", name="email").fill("hulu@gmail.com")
+    page.get_by_role("textbox", name="password").fill("hulu4789")
+
+    page.get_by_role("button", name="Register").last.click()
+    expect(page.locator("h3")).to_contain_text(re.compile("Conversion Page"))
+    
